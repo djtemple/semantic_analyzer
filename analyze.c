d9b24a3a313691964dbe38a16fbd4dbdc5ad72c6
Dylan T <dylan@Dylans-MacBook-Pro.local>
2019-03-12 11:06:29 -0600

    Added sample code

---
+++ analyze.c
@@ -0,0 +1,88 @@
+/****************************************************/
+/* File: analyze.c                                  */
+/* Semantic analyzer                                */
+/*                                                  */
+/*                                                  */
+/*                                                  */
+/****************************************************/
+
+#include "globals.h"
+#include "symtab.h"
+#include "analyze.h"
+
+
+static int scope_a = 0;
+
+/* counter for variable memory locations */
+static int location[MAX_SCOPE] = {0,0,0};
+
+static int No_change = 0; 
+/* Procedure traverse is a generic recursive 
+ * syntax tree traversal routine:
+ * it applies preProc in preorder and postProc 
+ * in postorder to tree pointed to by t
+ */
+static void traverse( TreeNode * t,
+               void (* preProc) (TreeNode *),
+               void (* postProc) (TreeNode *) )
+{ if (t != NULL)
+  { preProc(t);
+    int i;
+    for (i=0; i < MAXCHILDREN; i++)
+        traverse(t->child[i],preProc,postProc);
+    
+    postProc(t);
+    traverse(t->sibling,preProc,postProc);
+  }
+}
+
+/* nullProc is a do-nothing procedure to 
+ * generate preorder-only or postorder-only
+ * traversals from traverse
+ */
+static void nullProc(TreeNode * t)
+{ if (t==NULL) return;
+  else return;
+}
+
+/* Procedure insertNode inserts 
+ * identifiers stored in t into 
+ * the symbol table 
+ */
+static void insertNode( TreeNode * t)
+{ /* complete it */
+
+
+}
+
+/* Function buildSymtab constructs the symbol 
+ * table by preorder traversal of the syntax tree
+ */
+void buildSymtab(TreeNode * syntaxTree)
+{ traverse(syntaxTree,insertNode,nullProc);
+  if (TraceAnalyze)
+  { fprintf(listing,"\nSymbol table:\n\n");
+    printSymTab(listing);
+  }
+}
+
+static void typeError(TreeNode * t, char * message)
+{ fprintf(listing,"Type error at line %d: %s\n",t->lineno,message);
+  Error = TRUE;
+}
+
+/* Procedure checkNode performs
+ * type checking at a single tree node
+ */
+static void checkNode(TreeNode * t)
+{ /* complete it */
+
+
+}
+
+/* Procedure typeCheck performs type checking 
+ * by a postorder syntax tree traversal
+ */
+void typeCheck(TreeNode * syntaxTree)
+{ traverse(syntaxTree,nullProc,checkNode);
+}
