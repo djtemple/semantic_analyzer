1da18662c65970caf2b7d50ef54cc35f02ea5e36
Sanchuniathon <leeerisman@gmail.com>
2019-03-18 22:56:31 -0600

    inserted relevant TINY code into checkNone and insertNode - to be converted to C-

--- analyze.c
+++ analyze.c
@@ -44,16 +44,50 @@ static void nullProc(TreeNode * t)
 { if (t==NULL) return;
   else return;
 }
-
+///////////////////////////////////////////////////////////////////
 /* Procedure insertNode inserts 
  * identifiers stored in t into 
  * the symbol table 
  */
-static void insertNode( TreeNode * t)
-{ /* complete it */
-
+static void insertNode( TreeNode * t){ /* complete it */
+switch (t->nodekind)
+  { case StmtK:
+      switch (t->kind.stmt)
+      { case AssignK:
+        case ReadK:
+          if (st_lookup(t->attr.name) == -1)
+          /* not yet in table, so treat as new definition */
+            st_insert(t->attr.name,t->lineno,location++);
+          else
+          /* already in table, so ignore location, 
+             add line number of use only */ 
+            st_insert(t->attr.name,t->lineno,0);
+          break;
+        default:
+          break;
+      }
+      break;
+    case ExpK:
+      switch (t->kind.exp)
+      { case IdK:
+          if (st_lookup(t->attr.name) == -1)
+          /* not yet in table, so treat as new definition */
+            st_insert(t->attr.name,t->lineno,location++);
+          else
+          /* already in table, so ignore location, 
+             add line number of use only */ 
+            st_insert(t->attr.name,t->lineno,0);
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
 
 }
+/////////////////////////////////////////////////////////////////
 
 /* Function buildSymtab constructs the symbol 
  * table by preorder traversal of the syntax tree
@@ -71,14 +105,61 @@ static void typeError(TreeNode * t, char * message)
   Error = TRUE;
 }
 
+//////////////////////////////////////////////////////////////////////////////////
 /* Procedure checkNode performs
  * type checking at a single tree node
  */
 static void checkNode(TreeNode * t)
 { /* complete it */
+ switch (t->nodekind)
+  { case ExpK:
+      switch (t->kind.exp)
+      { case OpK:
+          if ((t->child[0]->type != Integer) ||
+              (t->child[1]->type != Integer))
+            typeError(t,"Op applied to non-integer");
+          if ((t->attr.op == EQ) || (t->attr.op == LT))
+            t->type = Boolean;
+          else
+            t->type = Integer;
+          break;
+        case ConstK:
+        case IdK:
+          t->type = Integer;
+          break;
+        default:
+          break;
+      }
+      break;
+    case StmtK:
+      switch (t->kind.stmt)
+      { case IfK:
+          if (t->child[0]->type == Integer)
+            typeError(t->child[0],"if test is not Boolean");
+          break;
+        case AssignK:
+          if (t->child[0]->type != Integer)
+            typeError(t->child[0],"assignment of non-integer value");
+          break;
+        case WriteK:
+          if (t->child[0]->type != Integer)
+            typeError(t->child[0],"write of non-integer value");
+          break;
+        case RepeatK:
+          if (t->child[1]->type == Integer)
+            typeError(t->child[1],"repeat test is not Boolean");
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
 
+  }
 
 }
+//////////////////////////////////////////////////////////////////////////////////
 
 /* Procedure typeCheck performs type checking 
  * by a postorder syntax tree traversal
