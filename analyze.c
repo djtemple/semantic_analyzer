80bf3f27bd97f65f4a3b2f4df9a2c96124714a46
lee.erisman1 <lee.erisman1@zone17-wa.cs.ucalgary.ca>
2019-03-22 13:15:43 -0600

    basic test case functionality, checkNode() working, scope and location for insertNode still problematic

--- analyze.c
+++ analyze.c
@@ -51,8 +51,103 @@ static void nullProc(TreeNode * t)
  */
 static void insertNode( TreeNode * t)
 { /* complete it */
-
-
+ switch (t->nodekind)
+  { case StmtK:
+      switch (t->kind.stmt)
+      { case IfK:
+          if (st_lookup(t->attr.name, 0) == -1)
+          /* not yet in table, so treat as new definition */
+            st_insert(t->attr.name,t->lineno, -1, 0 , t->isParameter);
+          else
+          /* already in table, so ignore location, 
+             add line number of use only */ 
+            st_insert(t->attr.name,t->lineno,No_change, 0 , t->isParameter);
+          break;
+        case WhileK:
+          if (st_lookup(t->attr.name, 0) == -1)
+          /* not yet in table, so treat as new definition */
+            st_insert(t->attr.name,t->lineno, -1, 0 , t->isParameter);
+          else
+          /* already in table, so ignore location, 
+             add line number of use only */ 
+            st_insert(t->attr.name,t->lineno,No_change, 0 , t->isParameter);
+          break;
+        case CallK:
+          if (st_lookup(t->attr.name, 0) == -1)
+          /* not yet in table, so treat as new definition */
+            st_insert(t->attr.name,t->lineno, -1, 0 , t->isParameter);
+          else
+          /* already in table, so ignore location, 
+             add line number of use only */ 
+            st_insert(t->attr.name,t->lineno,No_change, 0 , t->isParameter);
+          break;
+        case ReturnK:
+          if (st_lookup(t->attr.name, 0) == -1)
+          /* not yet in table, so treat as new definition */
+            st_insert(t->attr.name,t->lineno, -1, 0 , t->isParameter);
+          else
+          /* already in table, so ignore location, 
+             add line number of use only */ 
+            st_insert(t->attr.name,t->lineno,No_change, 0 , t->isParameter);
+          break;
+        default:
+            printf("stmt");
+          break;
+      }
+      break;
+    case ExpK:
+      switch (t->kind.exp)
+      { case IdK:
+          if (st_lookup(t->attr.name, 0) == -1)
+          /* not yet in table, so treat as new definition */
+            st_insert(t->attr.name,t->lineno, -1, 0 , t->isParameter);
+          else
+          /* already in table, so ignore location, 
+             add line number of use only */ 
+            st_insert(t->attr.name,t->lineno,No_change , 0, t->isParameter);
+          break;
+        default:
+          printf("expr");
+          break;
+      }
+      break;
+    case DecK:
+      switch (t->kind.dec)
+      { case VarK:
+          if (var_lookup(t->attr.name, 0) == -1)
+          /* not yet in table, so treat as new definition */
+            st_insert(t->attr.name,t->lineno, -1, 0 , t->isParameter);
+          else
+          /* already in table, so ignore location, 
+             add line number of use only */ 
+            st_insert(t->attr.name,t->lineno,No_change , 0, t->isParameter);
+          break;
+        case ArrayK:
+          if (st_lookup(t->attr.name, 0) == -1)
+          /* not yet in table, so treat as new definition */
+            st_insert(t->attr.name,t->lineno, -1, 0 , t->isParameter);
+          else
+          /* already in table, so ignore location, 
+             add line number of use only */ 
+            st_insert(t->attr.name,t->lineno, No_change , 0, t->isParameter);
+          break;
+        case FunK:
+          if (fun_lookup(t->attr.name, 0) == NULL)
+          /* not yet in table, so treat as new definition */
+            st_insert(t->attr.name,t->lineno, -1, 0 , t->isParameter);
+          else
+          /* already in table, so ignore location, 
+             add line number of use only */ 
+            st_insert(t->attr.name,t->lineno, No_change , 0, t->isParameter);
+          break;
+        default:
+          printf("declr");
+          break;
+      }
+      break;
+    default:
+      break;
+  }
 }
 
 /* Function buildSymtab constructs the symbol 
@@ -76,7 +171,56 @@ static void typeError(TreeNode * t, char * message)
  */
 static void checkNode(TreeNode * t)
 { /* complete it */
+ switch (t->nodekind)
+  { case ExpK:
+      switch (t->kind.exp)
+      { case OpK:
+          if ((t->child[0]->type != Integer) ||
+              (t->child[1]->type != Integer))
+            typeError(t,"Op applied to non-integer");
+          if ((t->attr.op == LT) || (t->attr.op == LE) || (t->attr.op == GT) || (t->attr.op == GE) || (t->attr.op == EQ) || (t->attr.op == NQ))
+            t->type = Boolean;
+          else
+            t->type = Integer;
+          break;
+        case AssignK:
+          if (t->child[0]->type != Integer)
+            typeError(t->child[0],"assignment of non-integer value");
+          break;
+        case ConstK:
+        case IdK:
+          t->type = Integer;
+          break;
+        default:
+          break;
+      }
+      break;
+    case StmtK:
+      switch (t->kind.stmt)
+      { case IfK:
+          if (t->child[0]->type == Integer)
+            typeError(t->child[0],"if test is not Boolean");
+          break;
+        case WhileK:
+          if (t->child[0]->type == Integer)
+            typeError(t->child[0],"while test is not Boolean");
+          break;
+        case CallK:
+          if (t->child[0]->type != Integer)
+            typeError(t->child[0],"parameter is a non-integer value");
+          break;
+        case ReturnK:
+          if (t->child[0]->type != Integer)
+            typeError(t->child[0],"return value is a non-integer");
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
 
+  }
 
 }
 
